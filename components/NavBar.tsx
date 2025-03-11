49356d5b09611982d0a8ebd72df988e0be0b12f4
Stefan <floydrise@gmail.com>
2025-03-11 15:40:09 +0200

    Changes to Input

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,12 @@
-import {CameraIcon, Heart, MenuIcon, SearchIcon, ShoppingBasket, Truck, User} from "lucide-react";
+import {
+  CameraIcon,
+  Heart,
+  MenuIcon,
+  SearchIcon,
+  ShoppingBasket,
+  Truck,
+  User,
+} from "lucide-react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
@@ -20,21 +28,23 @@ export const NavBar = () => {
       </div>
       <div className={"w-full"}>
         {/*Div top*/}
-        <div className={"flex justify-around py-8 items-center"}>
+        <div className={"flex justify-between py-6 items-center"}>
           <Link href={"/"}>
-          <Image
-            src={"/ikea_logo.svg"}
-            alt={"ikea logo"}
-            height={"90"}
-            width={"90"}
-            className={""}
-          />
+            <div className="w-24 h-10 flex items-center justify-center">
+              <Image
+                  src={"/ikea_logo.svg"}
+                  alt="ikea logo"
+                  width={90}
+                  height={90}
+                  className="w-full h-full"
+              />
+            </div>
           </Link>
           {/*Input*/}
-          <div className={"relative"}>
+          <div className={"hidden md:block ml-4 relative w-3xl"}>
             <Input
-              className={"w-180 pl-10  h-12 rounded-full bg-zinc-100 "}
-              placeholder={"Търсете продукти, идеи, стаи"}
+              className={"pl-10  h-12 rounded-full bg-zinc-100 "}
+              placeholder={"Търсете продукти, стаи, идеи ..."}
             />
             <div
               className="absolute inset-y-0 left-0 pl-3
@@ -52,30 +62,63 @@ export const NavBar = () => {
               </div>
             </div>
           </div>
-          <Link href={"#"} className={"flex justify-center items-center hover:bg-zinc-300 rounded-full py-2 px-6"}>
-            <User/>
-            <p className={"font-bold text-xs"}>Вход/Регистрация</p>
-          </Link>
-          <Link href={"#"} className={"flex justify-center items-center hover:bg-zinc-300 rounded-full p-2"}>
-            <Truck/>
-          </Link>
-          <Link href={"#"} className={"flex justify-center items-center hover:bg-zinc-300 rounded-full p-2"}>
-            <Heart/>
-          </Link>
-          <Link href={"#"} className={"flex justify-center items-center hover:bg-zinc-300 rounded-full p-2"}>
-            <ShoppingBasket/>
-          </Link>
+          <div className={"flex"}>
+            <Link
+              href={"#"}
+              className={
+                "flex justify-center gap-2 items-center hover:bg-zinc-300 rounded-full p-2 md:py-2 md:px-6"
+              }
+            >
+              <User />
+              <p className={"hidden lg:block font-bold text-xs"}>
+                Вход/Регистрация
+              </p>
+            </Link>
+            <Link
+              href={"#"}
+              className={
+                "flex justify-center items-center hover:bg-zinc-300 rounded-full p-2"
+              }
+            >
+              <Truck />
+            </Link>
+            <Link
+              href={"#"}
+              className={
+                "flex justify-center items-center hover:bg-zinc-300 rounded-full p-2"
+              }
+            >
+              <Heart />
+            </Link>
+            <Link
+              href={"#"}
+              className={
+                "flex justify-center items-center hover:bg-zinc-300 rounded-full p-2"
+              }
+            >
+              <ShoppingBasket />
+            </Link>
+          </div>
         </div>
         {/*Div bottom*/}
         <div className={"pb-2 border-b"}>
           <nav className={"flex gap-8"}>
-            <Link href={"#"} className={"font-bold text-base hover:underline decoration-0"}>
+            <Link
+              href={"#"}
+              className={"font-bold text-base hover:underline decoration-0"}
+            >
               Продукти
             </Link>
-            <Link href={"#"} className={"font-bold text-base hover:underline decoration-0"}>
+            <Link
+              href={"#"}
+              className={"font-bold text-base hover:underline decoration-0"}
+            >
               Стаи
             </Link>
-            <Link href={"#"} className={"font-bold text-base hover:underline decoration-0"}>
+            <Link
+              href={"#"}
+              className={"font-bold text-base hover:underline decoration-0"}
+            >
               Вдъхновение
             </Link>
           </nav>
